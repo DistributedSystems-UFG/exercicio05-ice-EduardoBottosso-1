fd13308182372ff7829ef1c9bf07e79b126af7d0
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-26 16:25:12 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,10 +3,19 @@ import Demo
  
 communicator = Ice.initialize(sys.argv)
 
-base = communicator.stringToProxy("SimplePrinter:default -p 11000")
-printer = Demo.PrinterPrx.checkedCast(base)
-if not printer:
+base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 54.234.163.189 -p 11000")
+base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 54.234.163.189 -p 11000")
+
+printer1 = Demo.PrinterPrx.checkedCast(base1)
+printer2 = Demo.PrinterPrx.checkedCast(base2)
+
+if (not printer1) or (not printer2):
     raise RuntimeError("Invalid proxy")
 
-printer.printString("Hello World!")
-printer.printUpperText("Hello World!")
+printer1.printString("Hello World from object 1!")
+printer1.printUpperText("Hello World from object 1!")
+
+printer2.printString("Hello World from object 2!")
+printer2.printUpperText("Hello World from object 2!")
+
+communicator.destroy()
