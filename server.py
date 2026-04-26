c026eb90f6dfe8651749df5fdda5fc4046f5f782
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-26 15:31:40 -0300

    Update server.py
    acrescentando novas funções auxiliares

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import sys, Ice
 import Demo
  
 class PrinterI(Demo.Printer):
-    def printString(self, s, current=None):
+    def addPrefix(self, s):
+        return "Servidor diz: " + s
+     def formatText(self, s):
+        return s.upper()
+     def printString(self, s, current=None):
+        s = self.formatText(s)
+        s = self.addPrefix(s)
         print(s)
 
 communicator = Ice.initialize(sys.argv) 
