681002c7cc965763ffa03552f46fd9cb3fa0b553
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-26 16:20:57 -0300

    Update server.py
    acrescentando objeto2

--- server.py
+++ server.py
@@ -11,12 +11,25 @@ class PrinterI(Demo.Printer):
     def printString(self, s, current=None):
         s = self.addPrefix(s.upper())
         print(s)
+     
+class PrinterII(Demo.Printer):
+    def addPrefix(self, s):
+        return "Servidor 2 diz: " + s
+     
+    def printUpperText(self, s, current=None):
+        print ("Objeto2: " ,s.upper())
+ 
+    def printString(self, s, current=None):
+        s = self.addPrefix(s.upper())
+        print(s)
 
 communicator = Ice.initialize(sys.argv) 
 
 adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
-object = PrinterI()
-adapter.add(object, communicator.stringToIdentity("SimplePrinter"))
+object1 = PrinterI()
+object2 = PrinterII()
+adapter.add(object1, communicator.stringToIdentity("SimplePrinter"))
+adapter.add(object2, communicator.stringToIdentity("SimplePrinter"))
 adapter.activate()
 
 communicator.waitForShutdown()
